5c6d20d1704462c753866d2933097093f643f43f
Rakhim Davletkaliyev <rakhim.davletkaliyev@meetiqm.com>
2023-12-01 11:33:27 +0200

    basics done

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.get('/all', async (c) => {
 	const { results } = await c.env.DB.prepare("SELECT * from items").run();
 	let list = ''
 	results.forEach((item: any) => {
-		list = list + `<li><a href="${item.url}">${item.title}</a> (${item.feedId})</li>`
+		list = list + `<li><a href="${item.url}">${item.title}</a> (${item.feed_id})</li>`
 	})
 	return c.html(
 		`${list}`)
@@ -54,8 +54,13 @@ app.post('/f/add', async (c) => {
 	// const r = fetchFeed(c)
 	const body = await c.req.parseBody();
 	const url = body['url'].toString();
-	await addFeed(c, url)
-	await fetchFeed(c, url)
+	const f = await addFeed(c, url)
+	console.log(f)
+	if (f['success'] === true) {
+		const feedId = await getFeedIdByRSSUrl(c, url)
+		console.log(feedId)
+		await fetchFeed(c, feedId)
+	}
 	return c.text("OK")
 });
 
@@ -78,6 +83,13 @@ app.post('/service/fetch_feed', async (c) => {
 	return await fetchFeed(c, url);
 });
 
+// INTERNAL FUNCTIONS
+
+async function getFeedIdByRSSUrl(c, rssUrl) {
+	const { results } = await c.env.DB.prepare("SELECT feed_id FROM feeds where rss_url = ?").bind(rssUrl).all()
+	return results[0]['feed_id']
+}
+
 async function addFeed(c, rssUrl) {
 	let req;
 	try {
@@ -86,7 +98,6 @@ async function addFeed(c, rssUrl) {
 		return
 	}
 	const rssText = await req.text();
-
 	const parser = new XMLParser();
 
 	let rssJson;
@@ -98,38 +109,21 @@ async function addFeed(c, rssUrl) {
 
 	const title = rssJson.rss.channel.title
 	const url = rssJson.rss.channel.link
-	await c.env.DB.prepare("INSERT INTO feeds (title, url, rss_url) values (?, ?, ?)").bind(title, url, rssUrl).run()
+	return await c.env.DB.prepare("INSERT INTO feeds (title, url, rss_url) values (?, ?, ?)").bind(title, url, rssUrl).all()
 }
 
-async function fetchFeed(c, url) {
+async function fetchFeed(c, feedId: Number) {
 	const parser = new XMLParser();
 
-	let req;
-	try {
-		req = await fetch(url);
-	} catch (err) {
-		c.status(422)
-		return c.body('Invalid URL')
-	}
+	const { results: feeds } = await c.env.DB.prepare("SELECT * FROM feeds WHERE feed_id = ?").bind(feedId).all();
+	const url = feeds[0]['rss_url']
+	const req = await fetch(url);
 	const rssText = await req.text();
-
-	let rssJson;
-	try {
-		rssJson = parser.parse(rssText, true);
-	} catch (err) {
-		c.status(422)
-		return c.body('Invalid RSS')
-	}
-
+	const rssJson = parser.parse(rssText, true);
 	const items = rssJson.rss.channel.item;
 
-	const { results } = await c.env.DB.prepare("SELECT * FROM feeds WHERE rss_url = ? LIMIT 1").bind(url).all();
-	const feedId = results[0]['feed_id'];
-
-	const { results: existingItems } = await c.env.DB.prepare(
-		"SELECT url FROM items WHERE feed_id = ?"
-	).bind(feedId).all();
-	const existingUrls = existingItems.map(obj => obj.Url);
+	const { results: existingItems } = await c.env.DB.prepare( "SELECT url FROM items WHERE feed_id = ?").bind(feedId).all();
+	const existingUrls = existingItems.map(obj => obj.url);
 
 	const filteredItems = items.filter(obj => !existingUrls.includes(obj.link));
 
@@ -140,15 +134,13 @@ async function fetchFeed(c, url) {
 		const stmt = c.env.DB.prepare("INSERT INTO items (feed_id, title, url) values (?, ?, ?)");
 
 		filteredItems.forEach((item: any) => {
-			binds.push(stmt.bind(1, item.title, item.link));
+			binds.push(stmt.bind(feedId, item.title, item.link));
 		});
 
 		await c.env.DB.batch(binds);
-		c.status(201);
-		return c.body('OK');
+		return 201
 	}
-	c.status(200);
-	return c.body('OK');
+	return 200
 }
 
 export default app
