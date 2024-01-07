f76ed6bda199def772a74a6adec7beed01d3ca5d
Rakhim Davletkaliyev <rakhim.davletkaliyev@meetiqm.com>
2024-01-07 13:57:38 +0200

    migration foundation

--- data.sql
+++ data.sql
@@ -1,1 +1,18 @@
-INSERT INTO feeds (feed_id, title, url, rss_url) VALUES (1, 'Rakhims blog', 'https://rakhim.org/', 'https://rakhim.org/index.xml');
+INSERT INTO users (user_id, username, password_hash, password_salt) VALUES 
+    (1, 'rakhim',  '94b7eb783a13ec4b0e0399951c78b94f5bc38917c714442aa22c87e12e0f629f', 'cf76f03fd6603ecd62b1380e0775cc48'),
+    (2, 'rakhim2', '58a3a9d3c4e5dd3cfe1304086f2485975acb65118551f5c23c296b0e33f33f44', 'd5f911d9fe8fb5278fa77a1370e77b14');
+
+INSERT INTO feeds (feed_id, title, url, rss_url) VALUES 
+    (1, 'Rakhim blog', 'https://rakhim.org', 'https://rakhim.org/index.xml'),
+    (2, 'Kevin Quirk', 'https://kevquirk.com', 'https://kevquirk.com/feed');
+
+INSERT INTO items (feed_id, title, description, content_html, url) VALUES 
+    (1, "Test 1", "Very test 1", "<strong>Very</strong> test 1", 'https://rakhim.org/test1'),
+    (1, "Test 2", "Very test 2", "<strong>Very</strong> test 2", 'https://rakhim.org/test2'),
+    (2, "KevQuirk 3", "Very KevQuirk 3", "Very <strong>QQQ</strong> 3", 'https://kevquirk.com/KevQuirk3'),
+    (2, "KevQuirk 4", "Very KevQuirk 4", "Very <strong>QQQ</strong> 3", 'https://kevquirk.com/KevQuirk4');
+
+INSERT INTO favorites (user_id, item_id) VALUES (1, 1);
+
+INSERT INTO followings (follower_user_id, followed_user_id) VALUES (1, 2);
+
--- schema.sql
+++ schema.sql
@@ -15,9 +15,6 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT, 
     password_salt TEXT
 );
-INSERT INTO users (user_id, username, password_hash, password_salt) VALUES 
-    (1, 'rakhim',  '94b7eb783a13ec4b0e0399951c78b94f5bc38917c714442aa22c87e12e0f629f', 'cf76f03fd6603ecd62b1380e0775cc48'),
-    (2, 'rakhim2', '58a3a9d3c4e5dd3cfe1304086f2485975acb65118551f5c23c296b0e33f33f44', 'd5f911d9fe8fb5278fa77a1370e77b14');
 
 CREATE TABLE IF NOT EXISTS sessions (
     session_id INTEGER PRIMARY KEY,
@@ -34,25 +31,19 @@ CREATE TABLE IF NOT EXISTS feeds (
     url TEXT UNIQUE NOT NULL, 
     rss_url Text UNIQUE NOT NULL
 );
-INSERT INTO feeds (feed_id, title, url, rss_url) VALUES 
-    (1, 'Rakhim blog', 'https://rakhim.org', 'https://rakhim.org/index.xml'),
-    (2, 'Kevin Quirk', 'https://kevquirk.com', 'https://kevquirk.com/feed');
 
 CREATE TABLE IF NOT EXISTS items (
 	item_id INTEGER PRIMARY KEY,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	feed_id INTEGER NOT NULL, 
 	title TEXT NOT NULL, 
-    content TEXT,
+    description TEXT,
+    content_html TEXT,
+    content_html_scraped TEXT,
 	url TEXT UNIQUE NOT NULL, 
     pub_date TIMESTAMP,
 	FOREIGN KEY(feed_id) REFERENCES feeds(feed_id)
 );
-INSERT INTO items (feed_id, title, content, url) VALUES 
-    (1, "Test 1", "Very test 1", 'https://rakhim.org/test1'),
-    (1, "Test 2", "Very test 2", 'https://rakhim.org/test2'),
-    (2, "KevQuirk 3", "Very KevQuirk 3", 'https://kevquirk.com/KevQuirk3'),
-    (2, "KevQuirk 4", "Very KevQuirk 4", 'https://kevquirk.com/KevQuirk4');
 
 CREATE TABLE IF NOT EXISTS favorites (
     favorite_id INTEGER PRIMARY KEY,
@@ -63,7 +54,6 @@ CREATE TABLE IF NOT EXISTS favorites (
     FOREIGN KEY (item_id) REFERENCES items(item_id),
     UNIQUE (user_id, item_id)
 );
-INSERT INTO favorites (user_id, item_id) VALUES (1, 1);
 
 CREATE TABLE subscriptions (
     subscription_id INTEGER PRIMARY KEY,
@@ -75,7 +65,6 @@ CREATE TABLE subscriptions (
     UNIQUE (user_id, feed_id)
 );
 
-
 CREATE TABLE followings (
     following_id INTEGER PRIMARY KEY,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -85,8 +74,5 @@ CREATE TABLE followings (
     FOREIGN KEY (followed_user_id) REFERENCES users(user_id),
     UNIQUE (follower_user_id, followed_user_id)
 );
-
-INSERT INTO followings (follower_user_id, followed_user_id) VALUES (1, 2);
-
 
 CREATE INDEX IF NOT EXISTS idx_users_on_username ON users(username);
