d927dfa0f1c4d35d827f9daf8ce63925daa406e4
Rakhim Davletkaliyev <hello@rakhim.org>
2024-10-02 21:51:36 +0300

    Revert "Try forcing content-type on feed extraction"
    This reverts commit c7f1ffa4fbd7a243b219d3ab7414c1ec0d63228a.

--- src/feed_extractor.ts
+++ src/feed_extractor.ts
@@ -1,50 +1,42 @@
 import { extract, FeedData } from "@extractus/feed-extractor";
 
 export async function extractRSS(RSSUrl: string): Promise<FeedData> {
-    return await extract(RSSUrl,
-        {
-            descriptionMaxLen: 0,
-            getExtraEntryFields: (feedEntry) => {
-                const {
-                    // this is plaintext of description, which may come from anything
-                    description: content_from_description,
+    return await extract(RSSUrl, {
+        descriptionMaxLen: 0,
+        getExtraEntryFields: (feedEntry) => {
+            const {
+                // this is plaintext of description, which may come from anything
+                description: content_from_description,
 
-                    // if both description and content present, `description` is taken from description and we lose content, so here we save it
-                    // also, content is for ATOM format
-                    content: content_from_content,
+                // if both description and content present, `description` is taken from description and we lose content, so here we save it
+                // also, content is for ATOM format
+                content: content_from_content,
 
-                    // this is for RSS format and RDF format
-                    "content:encoded": content_from_content_encoded,
+                // this is for RSS format and RDF format
+                "content:encoded": content_from_content_encoded,
 
-                    // for JSON format
-                    content_html: content_from_content_html,
+                // for JSON format
+                content_html: content_from_content_html,
 
-                    //
-                    pubdate: pubdate,
-                } = feedEntry as {
-                    description: string;
-                    content: string;
-                    "content:encoded": string;
-                    content_html: string;
-                    pubdate: string;
-                };
-                return {
-                    content_from_description,
-                    content_from_content_encoded,
-                    content_from_content,
-                    content_from_content_html,
-                    pubdate,
-                };
-            },
+                //
+                pubdate: pubdate,
+            } = feedEntry as {
+                description: string;
+                content: string;
+                "content:encoded": string;
+                content_html: string;
+                pubdate: string;
+            };
+            return {
+                content_from_description,
+                content_from_content_encoded,
+                content_from_content,
+                content_from_content_html,
+                pubdate,
+            };
         },
-        {
-            headers: {
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
-                'content-type': 'text/xml'
-            }
-        },
-    );
-
+    });
+    
 }
 
 export interface feedValidationResult {
