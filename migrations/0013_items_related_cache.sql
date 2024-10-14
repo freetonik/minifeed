fd642bf1fa82c36dffdd59c4b9f53bfc7ae9e3bd
Rakhim Davletkaliyev <hello@rakhim.org>
2024-10-14 18:37:57 +0300

    Change related cache migration table

--- migrations/0013_items_related_cache.sql
+++ migrations/0013_items_related_cache.sql
@@ -1,9 +1,8 @@
 -- Migration number: 0013 	 2024-10-13T15:05:09.317Z
 
 CREATE TABLE IF NOT EXISTS items_related_cache (
-	item_cache_id INTEGER PRIMARY KEY,
+	item_id INTEGER PRIMARY KEY,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	item_id INTEGER UNIQUE NOT NULL,
     content TEXT,
 	FOREIGN KEY(item_id) REFERENCES items(item_id) ON DELETE CASCADE
 );
