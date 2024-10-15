8e6970c2be4b2ed664460f167dcb83d741b7c535
Rakhim Davletkaliyev <hello@rakhim.org>
2024-10-15 14:40:30 +0300

    Lint ai and don't vectorize if content is empty after stripping tags

--- src/ai.ts
+++ src/ai.ts
@@ -1,101 +1,120 @@
-import { Context } from "hono";
-import { html, raw } from "hono/html";
-import { Bindings } from "./bindings";
-import { renderHTML } from "./htmltools";
-import { ItemRow } from "./interface";
-import { enqueueRegenerateItemRelatedCache, enqueueVectorizeStoreItem } from "./queue";
-import { stripNonLinguisticElements, stripTags } from "./utils";
+import type { Context } from 'hono';
+import { html, raw } from 'hono/html';
+import type { Bindings } from './bindings';
+import { renderHTML } from './htmltools';
+import type { ItemRow } from './interface';
+import { enqueueRegenerateItemRelatedCache, enqueueVectorizeStoreItem } from './queue';
+import { stripNonLinguisticElements } from './utils';
 
 export interface EmbeddingResponse {
     shape: number[];
     data: number[][];
 }
 
-export const vectorize_text = async (env:Bindings,  text: string): Promise<EmbeddingResponse> => {
-    return await env.AI.run(
-        "@cf/baai/bge-base-en-v1.5",
-        { text: [ text ], },
-    );
-}
+export const vectorize_text = async (env: Bindings, text: string): Promise<EmbeddingResponse> => {
+    return await env.AI.run('@cf/baai/bge-base-en-v1.5', { text: [text] });
+};
 
-export const add_vector_to_db = async (env:Bindings,  id: number, embeddings: EmbeddingResponse, namespace: string, metadata?: Record<string, VectorizeVectorMetadata>) => {
+export const add_vector_to_db = async (
+    env: Bindings,
+    id: number,
+    embeddings: EmbeddingResponse,
+    namespace: string,
+    metadata?: Record<string, VectorizeVectorMetadata>,
+) => {
     const vectors: VectorizeVector[] = [];
-    embeddings.data.forEach((vector) => {
-        vectors.push({ id: `${id}`, values: vector, namespace: namespace, metadata: metadata });
-    });
+    for (const vector of embeddings.data) {
+        vectors.push({
+            id: `${id}`,
+            values: vector,
+            namespace: namespace,
+            metadata: metadata,
+        });
+    }
     const upserted = await env.VECTORIZE.upsert(vectors);
     if (upserted) {
-        await env.DB.prepare( `REPLACE INTO items_vector_relation (item_id, vectorized) VALUES (?, ?)`, ).bind(id, 1).run();
+        await env.DB.prepare('REPLACE INTO items_vector_relation (item_id, vectorized) VALUES (?,?)').bind(id, 1).run();
     }
-}
-
-export const vectorize_and_store_item = async (env:Bindings,  item_id: number) => {
+};
+export const vectorize_and_store_item = async (env: Bindings, item_id: number) => {
     // if item is already in vector store, skip
     const vectors = await env.VECTORIZE.getByIds([`${item_id}`]);
     if (vectors.length) {
         // add to item_vector_relation table
-        await env.DB.prepare( `REPLACE INTO items_vector_relation (item_id, vectorized) VALUES (?, ?)`, ).bind(item_id, 1).run();
+        await env.DB.prepare('REPLACE INTO items_vector_relation (item_id, vectorized) VALUES (?, ?)')
+            .bind(item_id, 1)
+            .run();
         return;
     }
 
     const item = await env.DB.prepare(
-        `SELECT item_id, feed_id, title, description, content_html, content_html_scraped FROM items WHERE item_id = ?`,
-    ).bind(item_id).first<ItemRow>();
+        'SELECT item_id, feed_id, title, description, content_html, content_html_scraped FROM items WHERE item_id = ?',
+    )
+        .bind(item_id)
+        .first<ItemRow>();
     if (!item) return;
 
     let contentBlock = '';
-    
-    if (!item.description && !item.content_html && !item.content_html_scraped) { // We have nothing but title
+
+    if (!item.description && !item.content_html && !item.content_html_scraped) {
+        // We have nothing but title
         contentBlock = item.title;
-    } else { // We have some content
-        if (item.content_html) { // We have full content from feed
-            if (item.content_html_scraped) { // We have scraped content
-                if ( item.content_html.length > item.content_html_scraped.length * 0.65 ) {
-                        contentBlock = raw(item.content_html);
-                    } else {
-                        contentBlock = raw(item.content_html_scraped);
-                    }
-                } else { // No scraped content, use full content
+    } else {
+        // We have some content
+        if (item.content_html) {
+            // We have full content from feed
+            if (item.content_html_scraped) {
+                // We have scraped content
+                if (item.content_html.length > item.content_html_scraped.length * 0.65) {
                     contentBlock = raw(item.content_html);
+                } else {
+                    contentBlock = raw(item.content_html_scraped);
                 }
-        } else if (item.content_html_scraped) { // no full content, use scraped
+            } else {
+                // No scraped content, use full content
+                contentBlock = raw(item.content_html);
+            }
+        } else if (item.content_html_scraped) {
+            // no full content, use scraped
             contentBlock = raw(item.content_html_scraped);
-        }
-        else { // nothing but description
+        } else {
+            // nothing but description
             contentBlock = item.description;
         }
-        contentBlock = item.title + '. ' + await stripTags(await stripNonLinguisticElements(contentBlock));
+        if (contentBlock.length < 10) {
+            contentBlock = item.title;
+        } else {
+            const stripped = await stripNonLinguisticElements(contentBlock);
+            contentBlock = `${item.title}. ${stripped}`;
+        }
     }
 
-    const embeddings: EmbeddingResponse = await vectorize_text(env,  `${contentBlock}`);
+    const embeddings: EmbeddingResponse = await vectorize_text(env, `${contentBlock}`);
     await add_vector_to_db(
-        env, 
-        item.item_id, 
-        embeddings, 
-        "items", // namespace
+        env,
+        item.item_id,
+        embeddings,
+        'items', // namespace
         {
-            feed_id: item.feed_id // metadata
-        }
+            feed_id: item.feed_id, // metadata
+        },
     );
-}
-
+};
 export const handle_vectorize = async (c: Context) => {
     const env = c.env as Bindings;
-    const start = c.req.query("start");
-    const stop = c.req.query("stop");
+    const start = c.req.query('start');
+    const stop = c.req.query('stop');
 
     if (!start || !stop) {
-
-        const list = 'Here we will find unvectorized items maybe?'
-        return c.html(
-            renderHTML(`admin | minifeed`, html(list), c.get("USERNAME"), ""),
-        );
+        const list = 'Here we will find unvectorized items maybe?';
+        return c.html(renderHTML(`admin | minifeed`, html(list), c.get('USERNAME'), ''));
     }
 
-
     const items = await env.DB.prepare(
-        `SELECT item_id, title, item_sqid FROM items WHERE item_id >= ? AND item_id <= ?`,
-    ).bind(start, stop).all<ItemRow>();
+        'SELECT item_id, title, item_sqid FROM items WHERE item_id >= ? AND item_id <= ?',
+    )
+        .bind(start, stop)
+        .all<ItemRow>();
 
     let response = '<ol>';
     for (const item of items.results) {
@@ -104,18 +123,19 @@ export const handle_vectorize = async (c: Context) => {
     }
     response += '</ol>';
     return c.html(response);
-    
-}
+};
 
 export const handle_generate_related = async (c: Context) => {
     const env = c.env as Bindings;
-    const start = c.req.query("start");
-    const stop = c.req.query("stop");
-    if (!start || !stop) return c.text("No start or stop provided", 400);
+    const start = c.req.query('start');
+    const stop = c.req.query('stop');
+    if (!start || !stop) return c.text('No start or stop provided', 400);
 
     const items = await env.DB.prepare(
         `SELECT item_id, title, item_sqid FROM items WHERE item_id >= ? AND item_id <= ?`,
-    ).bind(start, stop).all<ItemRow>();
+    )
+        .bind(start, stop)
+        .all<ItemRow>();
 
     let response = '<ol>';
     for (const item of items.results) {
@@ -124,5 +144,4 @@ export const handle_generate_related = async (c: Context) => {
     }
     response += '</ol>';
     return c.html(response);
-
-}
+};
