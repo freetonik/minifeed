51f01928dfa9a86b81c5923d0ebd1513627b9e78
Rakhim Davletkaliyev <hello@rakhim.org>
2024-12-20 14:35:53 +0200

    Show item descriptions in lists

--- src/handlers/lists/list.ts
+++ src/handlers/lists/list.ts
@@ -16,7 +16,7 @@ export const handleListsSingle = async (c: Context) => {
             WHERE list_id = ?`,
         ).bind(listId),
         c.env.DB.prepare(
-            `SELECT items.item_sqid, items.title, items.url, items.pub_date, feeds.title as feed_title, feeds.feed_sqid
+            `SELECT items.item_sqid, items.title, items.url, items.pub_date, feeds.title as feed_title, feeds.feed_sqid, items.description
             FROM item_list_items
             JOIN items ON items.item_id = item_list_items.item_id
             JOIN feeds ON items.feed_id = feeds.feed_id
@@ -28,14 +28,22 @@ export const handleListsSingle = async (c: Context) => {
     if (!listEntry) return c.notFound();
 
     let inner = `<h1 style="margin-bottom:0">${listEntry.title}</h1>
-    <p style="margin: 0.75em 0 3em;">list by <a href="/users/${listEntry.username}">@${listEntry.username}</a></p>`;
+    <p style="margin: 0.75em 0 2em;">list by <a href="/users/${listEntry.username}">@${listEntry.username}</a></p>`;
     for (const item of list_items.results) {
-        inner += renderItemShort(item.item_sqid, item.title, item.url, item.feed_title, item.feed_sqid, item.pub_date);
+        inner += renderItemShort(
+            item.item_sqid,
+            item.title,
+            item.url,
+            item.feed_title,
+            item.feed_sqid,
+            item.pub_date,
+            item.description,
+        );
     }
 
     if (userId === listEntry.user_id) {
         inner += `<p>
-            <button hx-confirm="Are you sure?" hx-swap="outerHTML transition:true" hx-post="/lists/${listSqid}/delete">Delete list</button>
+            <button class="button" hx-confirm="Are you sure?" hx-swap="outerHTML transition:true" hx-post="/lists/${listSqid}/delete">Delete list</button>
         </p>`;
     }
 
