01f1c659eeda7a419e06450f2a6c3c34e47973e5
Rakhim Davletkaliyev <hello@rakhim.org>
2024-12-24 15:06:54 +0200

    Only show users which have at least 1 favorite or at least 1 list

--- src/handlers/users/users.ts
+++ src/handlers/users/users.ts
@@ -5,7 +5,13 @@ import { renderGuestFlash, renderHTML } from '../../htmltools';
 export async function handleUsers(c: Context) {
     const username = c.get('USERNAME');
     const { results } = await c.env.DB.prepare(
-        'SELECT * from users WHERE users.email_verified = 1 ORDER BY created ASC ',
+        `SELECT * from users
+        LEFT JOIN favorites ON favorites.user_id = users.user_id
+        LEFT JOIN item_lists ON item_lists.user_id = users.user_id
+        WHERE
+            users.email_verified = 1
+            AND (item_lists.list_id IS NOT NULL OR favorites.favorite_id IS NOT NULL)
+        ORDER BY created ASC `,
     ).run();
 
     let inner = '';
