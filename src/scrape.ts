c8ed78fc5d066d29229f9eb41d587a158a2b9ca6
Rakhim Davletkaliyev <hello@rakhim.org>
2025-02-16 15:09:45 +0200

    Check robots on each scrape attempt, closes #76

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,9 +1,22 @@
 import { Readability } from '@mozilla/readability';
 import { parseHTML } from 'linkedom';
 import type { ArticleInfo } from './interface';
+import { getRobots } from './utils';
 
 export async function scrapeURLIntoObject(url: string): Promise<ArticleInfo> {
-    const response = await fetch(url);
+    const robots = await getRobots(url);
+    if (robots) {
+        const iaArchiverAllowed = robots.isAllowed(url, 'ia_archiver');
+        const minifeedAllowed = robots.isAllowed(url, 'minifeed_archiver');
+        if (!iaArchiverAllowed || !minifeedAllowed) throw new Error('robots.txt disallows archiving');
+    }
+
+    const response = await fetch(url, {
+        headers: {
+            'User-Agent': 'minifeed_archiver',
+        },
+    });
+
     const html = await response.text();
     const { document } = parseHTML(html);
     const cleanArticleContent = new Readability(document).parse();
