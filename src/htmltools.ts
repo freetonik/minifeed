e7126bc424569b617b4b738a8e462b05bf23019a
Rakhim Davletkaliyev <hello@rakhim.org>
2025-10-06 09:09:47 +0300

    Serve CSS via assets

--- src/htmltools.ts
+++ src/htmltools.ts
@@ -1,6 +1,5 @@
 import type { Context } from 'hono';
 import { html, raw } from 'hono/html';
-import { renderedCSS } from './css';
 import type { FeedSearchResult, ItemSearchResult } from './interface';
 
 export function renderHTML(c: Context, title: string, inner: string, debugInfo = '') {
@@ -36,9 +35,7 @@ export function renderHTML(c: Context, title: string, inner: string, debugInfo =
 
         ${canonicalUrlBlock}
 
-        <style>
-        ${renderedCSS}
-        </style>
+        <link rel="stylesheet" href="/assets/style.css">
 
         <link rel="shortcut icon" href="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ij48cGF0aCBkPSJtMTIuNjcyLjY2OCAzLjA1OSA2LjE5NyA2LjgzOC45OTNhLjc1Ljc1IDAgMCAxIC40MTYgMS4yOGwtNC45NDggNC44MjMgMS4xNjggNi44MTJhLjc1Ljc1IDAgMCAxLTEuMDg4Ljc5TDEyIDE4LjM0N2wtNi4xMTYgMy4yMTZhLjc1Ljc1IDAgMCAxLTEuMDg4LS43OTFsMS4xNjgtNi44MTEtNC45NDgtNC44MjNhLjc0OS43NDkgMCAwIDEgLjQxNi0xLjI3OWw2LjgzOC0uOTk0TDExLjMyNy42NjhhLjc1Ljc1IDAgMCAxIDEuMzQ1IDBaIj48L3BhdGg+PC9zdmc+" />
 
